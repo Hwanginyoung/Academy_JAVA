4d1c3f29b18c75cb8e3dcdf2eee89d7ac2aa9bfa
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-01-19 18:22:54 +0900

    Scanner

---
+++ src/Ch04/C01Scanner.java
@@ -0,0 +1,11 @@
+
+public class C01Scanner {
+
+	public static void main(String[] args) {
+		//System.out :출력스트림 형성(해석 :System이 가지고 있는 출력장치(out))- ex.모니터
+		//System.in  :입력스트림 형성(해석 :System이 가지고 있는 입력장치(in))- ex. 키보드,마우스
+		
+
+	}
+
+}
