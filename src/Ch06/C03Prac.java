159866378c1f254dd520039cb77385c3a2f2b34f
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-01-24 18:21:25 +0900

    else if practice

---
+++ src/Ch06/C03Prac.java
@@ -0,0 +1,48 @@
+package Ch06;
+
+public class C03Prac {
+
+	public static void main(String[] args) {
+		
+		//학점을 입력 받아서
+		//90점 이상이면 A
+		//80점 이상이면 B
+		//70점 이상이면 C
+		//60점 이상이면 D
+		//60점 미만이면 F 를 출력
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("학점 : ");
+//		int score = sc.nextInt();
+//		
+//		if(score>=90) {
+//			System.out.println("A")
+//		} else if(score>=80) {
+//			System.out.println("B")
+//		} else if(score>=70) {
+//			System.out.println("c")
+//		} else if(score>=60) {
+//			System.out.println("D")
+//		} else {
+//			System.out.println("F")
+//		}
+		
+		
+		//나이를 입력받습니다
+		//20이상이면 성인입니다 출력
+		//8살 이상이면 취학아동 입니다
+		//8살 미만이면 미취학아동 입니다
+		Scanner sc = new Scanner(System.in);
+		System.out.print("나이 : ")
+		int age - sc.nextInt();
+		
+		if(age>=20) {
+			System.out.println("성인");
+		} else if(age>8) {
+			System.out.println("취학아동");
+		} else {
+			System.out.println("미취학아동");
+		}
+
+	}
+
+}
