82b27cf187bbdf138cfb8c1cdcebc258c75c679f
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-01-24 18:47:27 +0900

    중첩 if문 practice

--- src/Ch06/C05Prac.java
+++ src/Ch06/C05Prac.java
@@ -5,8 +5,9 @@ public class C05Prac {
 	public static void main(String[] args) {
 		
 		//정수 하나 입력 받습니다
-		//3의 배수 이면서 5의 배수라면 출력
+		//3의 배수이면서 5의 배수라면 출력
 		//3의 배수이면서 5의 배수는 아니면서 4의 배수라면 출력
+		
 		//3의 배수가 아니면서 4의 배수라면 출력
 		//&&연산자는 쓰지 않습니다.
 		Scanner sc = new Scanner(system.in);
