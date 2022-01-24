3845069633309de6cddabeca3d3398fe89ad1517
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-01-24 18:20:08 +0900

    else if

--- src/Ch06/C02IF.java
+++ src/Ch06/C02IF.java
@@ -11,28 +11,28 @@ public class C02IF {
 		//수학점수 : 40점이상
 		//전체 평균이 60점이상이면 합격
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.print("국 영 수 입력 : ");
-		int kor = sc.nextInt();
-		int eng = sc.nextInt();
-		int mat = sc.nextInt();
-		int avg = (kor+eng+mat)/3;
-		
-		if(kor<40) {
-			System.out.println("불합격");
-		} 
-		else if(eng<40) { //kor점수가 40점 이상인 경우
-			System.out.println("불합격");
-		} 
-		else if(mat<40) { //kor점수와 eng점수가 40점 이상인 경우
-			System.out.println("불합격");
-		} 
-		else if(avg<60) { //kor,eng,mat점수가 40점 이상인 경우
-			System.out.println("불합격")
-		} 
-		else { //평균이 60점 이상인 경우
-			System.out.println("합격");
-		}
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("국 영 수 입력 : ");
+//		int kor = sc.nextInt();
+//		int eng = sc.nextInt();
+//		int mat = sc.nextInt();
+//		int avg = (kor+eng+mat)/3;
+//		
+//		if(kor<40) {
+//			System.out.println("불합격");
+//		} 
+//		else if(eng<40) { //kor점수가 40점 이상인 경우
+//			System.out.println("불합격");
+//		} 
+//		else if(mat<40) { //kor점수와 eng점수가 40점 이상인 경우
+//			System.out.println("불합격");
+//		} 
+//		else if(avg<60) { //kor,eng,mat점수가 40점 이상인 경우
+//			System.out.println("불합격")
+//		} 
+//		else { //평균이 60점 이상인 경우
+//			System.out.println("합격");
+//		}
 
 	}
 
