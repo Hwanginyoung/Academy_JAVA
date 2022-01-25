fccb6c666ee168aa71018ca3db1d7a7c5834f838
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-01-25 18:46:00 +0900

    중첩while(구구단)

---
+++ src/Ch07/C02While.java
@@ -0,0 +1,47 @@
+package Ch07;
+
+public class C02While {
+
+	public static void main(String[] args) {
+
+		//중첩 while 기본
+		//전체 구구단 출력
+		//2단 ~ 9단
+		
+		//
+		
+//		int dan=2;
+//		int i=1;
+//		
+//		while(dan<=9) {
+//			i=1; //초기값
+//			
+//			while(i<=9) { //조건식
+//				System.out.println(dan+" X "+i+" = "(dan*i));
+//				i++; //탈출연산
+//			}
+//			System.out.println();
+//			dan++;
+//		}
+		
+		
+		
+		//전체 구구단 역순출력
+//		int dan=9;
+//		int i=9;
+//		
+//		while(dan>=1) {
+//			i=9;
+//			
+//			while(i>=1) {
+//				System.out.println(dan+" X "+i+" = "(dan*i));
+//				i--;
+//			}
+//			System.out.println();
+//			dan--;
+//		}
+		
+
+	}
+
+}
