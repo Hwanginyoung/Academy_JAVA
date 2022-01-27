8bed9304ae6b7f553c056f71b7161b2038155caa
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-01-27 18:00:10 +0900

    break, continue

---
+++ src/Ch07/C03While.java
@@ -0,0 +1,68 @@
+package Ch07;
+
+public class C03While {
+
+	public static void main(String[] args) {
+
+
+		//break : 반복문 벗어날때(가장 근접한 반복먼 or Switch문을 벗어남
+		
+//		int cnt=0;
+//		while(true) {
+//			System.out.println("Cnt : "+ (cnt++));
+//			Thread.slee(500);
+//			if(cnt==25) {
+//				break;
+//			}
+//		}
+		
+		//문제
+		//정수를 -1을 입력받기 전까지 무한대로 입력받아 그 정수들의 총합 출력
+//		Scanner sc = new Scanner(System.in);
+//		int sum=0;//총합
+//		int n=0;//키보드로 입력받기
+//		while(true) {
+//			System.out.print("입력(종료-1) : ");
+//			n=sc.nextInt();
+//			if(n==-1) {
+//				break;
+//			}
+//			sum+=n;
+//		}
+//		System.out.println("총합 : "+sum);
+		
+		
+		
+		//continue 반복문의 조건식으로 이동
+		
+//		int cnt=0;
+//		while(cnt<=20) {
+//			
+//			System.out.println("CNT : "+(cnt));
+//			Thread.sleep(500);
+//			if(nct==15) {
+//				//cnt++;
+//				continue;//반복문의 조건식으로 돌아감
+//			}
+//			cnt++;
+//		}
+		
+		//1부터 20까지의 수중의 3의 배수를 제위하고 나머지 숫자들을 출력
+//		int i=1;
+//		while(i<=20) {
+//			
+//			if(i%3==0) {
+//				i++;
+//				continue;
+//			}
+//			
+//			System.out.println("i : "+i);
+//			i++;
+//			Thread.sleep(500);
+//		}
+		
+
+		
+	}
+
+}
