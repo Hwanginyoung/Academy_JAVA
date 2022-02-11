83a58b5b44f6b7f9a03ecd520a994b75967360e3
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-02-11 18:40:19 +0900

    Final

--- src/Ch14/C06FinalMain.java
+++ src/Ch14/C06FinalMain.java
@@ -14,10 +14,14 @@ public class C06FinalMain {
 		C06Simple obj = new C06Simple();
 		
 //		obj.x=10;
-		obj.y=20;
+//		obj.y=20;
 
 		final int num=10;
 //		num=20;
+		
+		System.out.println("MAX : " + Define.MAX);
+		System.out.println("MIN : " + Define.MIN);
+		System.out.println("PI : " + Define.PI);
 		
 	}
 
