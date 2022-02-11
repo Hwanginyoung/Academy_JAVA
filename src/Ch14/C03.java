1300cf2c6932ac61a5d7dacb31afb090c70775a6
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-02-11 17:36:37 +0900

    오버라이딩(함수재정의), upcasting

---
+++ src/Ch14/C03.java
@@ -0,0 +1,31 @@
+package Ch14;
+
+class C03Animal{
+	void Sound() {System.out.println("소리를 냅니다!");}
+}
+class C03Dog extends C03Animal{
+	void Sound() {System.out.println("멍멍 소리를 냅니다!");} //오버라이딩(함수재정의)
+}
+class C03Cat extends C03Animal{
+	void Sound() {System.out.println("야옹 소리를 냅니다!");} //오버라이딩(함수재정의)
+}
+
+public class C03 {
+	public static void main(String[] args) {
+	
+		C03Animal ani=null;
+		
+		ani=new C03Dog();	//Upcasting
+		ani.Sound();
+		System.out.println();
+		ani=new C03Cat();	//Upcasting
+		ani.Sound();
+		
+		//Upcasting 정리
+		//상속관계에서 상위클래스의 참조변수가 확장된 멤버에 접근할 수 있다? X
+		//-> DownCasting을 통해서 각 하위클래스의 멤버에 접근!
+		
+		//상속관계에서 상위클래스의 참조변수가 오버라이딩된 멤버에 접근할 수 있다? O		
+		
+	}
+}
