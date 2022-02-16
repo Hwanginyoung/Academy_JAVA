93ea7785595a1fb68ae2bad61d62ba5dc08ceed5
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-02-16 18:39:50 +0900

    generic

---
+++ src/Ch20/C01Generic.java
@@ -0,0 +1,29 @@
+package Ch20;
+
+class Simple <T> {
+	public T x;
+	public T y;
+	Simple(T x, T y) {
+		this.x = x; this.y = y;
+	}
+	@Override
+	public String toString() {
+		return "Simple [x=" + x + ", y=" + y + "]";
+	}
+}
+
+public class C02Generic {
+	public static void main(String[] args) {
+
+		Simple<Integer> ob = new Simple(10,20);
+		System.out.println("ob1 : "+ ob1.toString());
+		
+		Simple<Double> ob2 = new Simple(10.1, 20.1);
+		System.out.println("ob2 : "+ ob2.toString());
+		
+		Simple<String> ob3 = new Simple("하나", "둘");
+		System.out.println("ob3 : "+ ob3.toString());
+
+	}
+
+}
